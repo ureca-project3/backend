285421a8dd2aa83b9348e8af6971f887f6867cf1
LeeDoRim <65598286+LeeDoRim@users.noreply.github.com>
2024-10-18 20:36:51 +0900

    feat: 자녀 성향 진단 질문 조회

--- src/main/java/com/triple/backend/test/repository/TestRepository.java
+++ src/main/java/com/triple/backend/test/repository/TestRepository.java
@@ -1,4 +1,10 @@
 package com.triple.backend.test.repository;
 
-public class TestRepository {
+import com.triple.backend.test.entity.Test;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TestRepository extends JpaRepository<Test, Long> {
+
 }
--- src/main/java/com/triple/backend/test/service/impl/TestServiceImpl.java
+++ src/main/java/com/triple/backend/test/service/impl/TestServiceImpl.java
@@ -1,4 +1,39 @@
 package com.triple.backend.test.service.impl;
 
-public class TestServiceImpl {
+import com.triple.backend.test.dto.TestQuestionResponseDto;
+import com.triple.backend.test.entity.Test;
+import com.triple.backend.test.entity.TestQuestion;
+import com.triple.backend.test.repository.TestQuestionRepository;
+import com.triple.backend.test.repository.TestRepository;
+import com.triple.backend.test.service.TestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TestServiceImpl implements TestService {
+
+     private final TestRepository testRepository;
+
+     private final TestQuestionRepository testQuestionRepository;
+
+    @Override
+    public TestQuestionResponseDto getTestQuestion(Long testId) {
+
+        Test test = testRepository.findById(testId).orElseThrow( () -> new IllegalArgumentException("테스트 정보를 찾을 수 없습니다."));
+
+        List<TestQuestion> testQuestionList = testQuestionRepository.findByTest(test);
+
+        List<String> questionList = new ArrayList<>();
+
+        for (TestQuestion testQuestion : testQuestionList) {
+            questionList.add(testQuestion.getQuestionText());
+        }
+
+        return new TestQuestionResponseDto(test.getName(), test.getDescription(), questionList);
+    }
+
 }
