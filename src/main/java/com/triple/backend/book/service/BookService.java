fb606981bd84134ed81b51ba386f18fed4b3333e
leeseunghee00 <lsh9246@gmail.com>
2024-10-18 17:29:20 +0900

    feat: 도서 정보 상세 조회 구현

--- src/main/java/com/triple/backend/book/service/BookService.java
+++ src/main/java/com/triple/backend/book/service/BookService.java
@@ -1,4 +1,9 @@
 package com.triple.backend.book.service;
 
+import com.triple.backend.book.dto.BookDetailResponseDto;
+
 public interface BookService {
+
+	// 도서 정보 상세 조회
+	BookDetailResponseDto getBookDetail(Long bookId);
 }
