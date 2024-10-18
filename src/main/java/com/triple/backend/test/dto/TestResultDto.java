4a3cc384d1230924b9a1bb0a6d27c50017f5bca1
LeeDoRim <65598286+LeeDoRim@users.noreply.github.com>
2024-10-18 20:33:08 +0900

    refactor: Dto 코드 리펙토링

--- src/main/java/com/triple/backend/test/dto/TestResultDto.java
+++ src/main/java/com/triple/backend/test/dto/TestResultDto.java
@@ -1,12 +1,19 @@
 package com.triple.backend.test.dto;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@Data
+@Getter
+@NoArgsConstructor
 public class TestResultDto {
 
     List<TraitDataDto> traitDataDtoList;
+
+    public TestResultDto(List<TraitDataDto> traitDataDtoList) {
+        this.traitDataDtoList = traitDataDtoList;
+    }
 
 }
