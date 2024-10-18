b6521d82acf23483a55570b315b4067dc4b85c79
LeeDoRim <65598286+LeeDoRim@users.noreply.github.com>
2024-10-18 17:12:41 +0900

    feat: 성향 진단 관련 Dto 추가

---
+++ src/main/java/com/triple/backend/test/dto/TestResultDto.java
@@ -0,0 +1,12 @@
+package com.triple.backend.test.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class TestResultDto {
+
+    List<TraitDataDto> traitDataDtoList;
+
+}
---
+++ src/main/java/com/triple/backend/test/dto/TraitDataDto.java
@@ -0,0 +1,13 @@
+package com.triple.backend.test.dto;
+
+import lombok.Data;
+
+@Data
+public class TraitDataDto {
+
+    String traitName;
+    String traitDescription;
+    Integer traitScore;
+
+
+}
