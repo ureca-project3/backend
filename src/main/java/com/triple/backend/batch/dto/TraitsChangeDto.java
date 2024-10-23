649a2d8ea751c46e773b5e8ba8010fd2c7e9395f
kuma0112 <lsj23871@gmail.com>
2024-10-24 00:46:50 +0900

    feat: Feedback 배치 처리 관련 Dto 생성

---
+++ src/main/java/com/triple/backend/batch/dto/TraitsChangeDto.java
@@ -0,0 +1,15 @@
+package com.triple.backend.batch.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class TraitsChangeDto {
+    private Long childId;
+    private Long traitId;
+    private Double changeAmount;
+    private boolean isBeyondFive;
+}
