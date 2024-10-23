90a3799581ef9bee7be738a636e7c9f8dfd99b24
kuma0112 <lsj23871@gmail.com>
2024-10-24 00:46:27 +0900

    feat: DataSourceConfig 생성

---
+++ src/main/java/com/triple/backend/batch/config/DataSourceConfig.java
@@ -0,0 +1,35 @@
+package com.triple.backend.batch.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class DataSourceConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource.batch.hikari")
+    public DataSource batchDataSource() {
+        return DataSourceBuilder
+                .create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.main.hikari")
+    public DataSource mainDataSource() {
+        return DataSourceBuilder
+                .create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+}
