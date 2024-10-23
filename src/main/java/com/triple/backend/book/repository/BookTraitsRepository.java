d80fc510c31abfe6a35ffccc8ade2074676c4c03
kuma0112 <lsj23871@gmail.com>
2024-10-24 00:47:55 +0900

    refactor: BookTraitsRepository findByBookId -> findAllByBookId로 수정

--- src/main/java/com/triple/backend/book/repository/BookTraitsRepository.java
+++ src/main/java/com/triple/backend/book/repository/BookTraitsRepository.java
@@ -4,10 +4,10 @@ import com.triple.backend.book.entity.BookTraits;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface BookTraitsRepository extends JpaRepository<BookTraits,Long> {
 
-    @Query("select bt from BookTraits bt where bt.book.bookId = :bookId")
-    Optional<BookTraits> findByBookId(Long bookId);
+    @Query(value = "select bt from BookTraits bt where bt.book.bookId = :bookId")
+    List<BookTraits> findAllByBookId(Long bookId);
 }
