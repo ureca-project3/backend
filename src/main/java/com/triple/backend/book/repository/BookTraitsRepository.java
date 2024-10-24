dc8783fdf8a80b1dbdc05f601f814fe3d2aa76fc
leeseunghee00 <lsh9246@gmail.com>
2024-10-24 23:50:26 +0900

    refactor: 사용하지 않는 쿼리 메소드 제거

--- src/main/java/com/triple/backend/book/repository/BookTraitsRepository.java
+++ src/main/java/com/triple/backend/book/repository/BookTraitsRepository.java
@@ -1,13 +1,8 @@
 package com.triple.backend.book.repository;
 
-import com.triple.backend.book.entity.BookTraits;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
+import com.triple.backend.book.entity.BookTraits;
 
 public interface BookTraitsRepository extends JpaRepository<BookTraits,Long> {
-
-    @Query(value = "select bt from BookTraits bt where bt.book.bookId = :bookId")
-    List<BookTraits> findAllByBookId(Long bookId);
 }
