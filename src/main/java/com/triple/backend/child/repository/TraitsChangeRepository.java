f816d22290065f47b739714e71a369c9675c7f04
leeseunghee00 <lsh9246@gmail.com>
2024-10-24 23:27:44 +0900

    refactor: 사용하지 않는 쿼리 메소드 제거

--- src/main/java/com/triple/backend/child/repository/TraitsChangeRepository.java
+++ src/main/java/com/triple/backend/child/repository/TraitsChangeRepository.java
@@ -1,13 +1,8 @@
 package com.triple.backend.child.repository;
 
-import com.triple.backend.child.entity.TraitsChange;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
+import com.triple.backend.child.entity.TraitsChange;
 
 public interface TraitsChangeRepository extends JpaRepository<TraitsChange, Long> {
-
-    @Query(value = "select tc from TraitsChange tc where tc.child.childId = :childId and tc.trait.traitId = :traitId")
-    Optional<TraitsChange> findByChildIdAndTraitId(Long childId, Long traitId);
 }
