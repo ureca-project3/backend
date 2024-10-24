bedfa1e0777c0083f7ad254b9e265d6f3ba19ee6
leeseunghee00 <lsh9246@gmail.com>
2024-10-24 23:49:10 +0900

    refactor: FeedbackProcessor 프로세스 로직 변경

--- src/main/java/com/triple/backend/batch/tasklet/FeedbackProcessor.java
+++ src/main/java/com/triple/backend/batch/tasklet/FeedbackProcessor.java
@@ -1,83 +1,103 @@
 package com.triple.backend.batch.tasklet;
 
-import com.triple.backend.batch.dto.FeedbackDto;
-import com.triple.backend.batch.dto.TraitsChangeDto;
-import com.triple.backend.book.entity.BookTraits;
-import com.triple.backend.book.repository.BookTraitsRepository;
-import com.triple.backend.child.entity.ChildTraits;
-import com.triple.backend.child.repository.ChildTraitsRepository;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.triple.backend.batch.dto.BookChildTraitDto;
+import com.triple.backend.batch.dto.TraitsChangeDto;
+import com.triple.backend.child.entity.ChildTraits;
+import com.triple.backend.child.repository.ChildTraitsRepository;
+import com.triple.backend.common.exception.NotFoundException;
+
+import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
-public class FeedbackProcessor implements ItemProcessor<List<FeedbackDto>, List<TraitsChangeDto>> {
+public class FeedbackProcessor implements ItemProcessor<BookChildTraitDto, TraitsChangeDto> {
 
     private final ChildTraitsRepository childTraitsRepository;
-    private final BookTraitsRepository bookTraitsRepository;
 
     @Override
-    public List<TraitsChangeDto> process(List<FeedbackDto> feedbackDtos) throws Exception {
-        List<TraitsChangeDto> traitsChangeDtoList = new ArrayList<>();
+    public TraitsChangeDto process(BookChildTraitDto bookChildTraitDto) throws Exception {
+        List<int[]> likedBookTraitsList = bookChildTraitDto.getLikedBookTraitsList();
+        List<int[]> hatedBookTraitsList = bookChildTraitDto.getHatedBookTraitsList();
 
-        /*
-        문제1 : feedbackDto를 돌면서 계속 똑같은 child의 trait를 찾아 비효율적이다.
-        문제2 : 한 멤버가 '좋아요'한 책들을 한꺼번에 가져오지 않고 각각 쿼리를 날려 비효율적이다.
-        문제3 : childTrait <-> bookTrait 1대1로 비교를 하는데, 책들끼리 성향 비교 후 멤버와 최종 비교해서 변화량을 측정하는 게 더 효율적이다.
-        문제4 : redis에 책들의 성향이 모두 저장되어 있다면(캐시되어 있다면), 훨씬 빠른 연산이 가능하다. mysql 접근을 줄일 수 없을까?
-        문제5 : ChildTraits, BookTraits의 성향 점수들은 Integer 타입이다. 하지만 성향 가중치는 소수점 단위로 올라간다. 현재는 Math.floor로 내림한 뒤, int 변환하는데, 더 좋은 방법이 있을까?
-         */
-        for (FeedbackDto feedbackDto : feedbackDtos) {
-            List<ChildTraits> childTraits = childTraitsRepository.findAllByChildId(feedbackDto.getChildId());
-            List<BookTraits> bookTraits = bookTraitsRepository.findAllByBookId(feedbackDto.getBookId());
+        int[] bookTrait = calculateBookTraits(likedBookTraitsList, hatedBookTraitsList);
+        int[] childTrait = bookChildTraitDto.getChildTraitScore();
 
-            double totalChangeAmount = 0.0;
-            for (BookTraits bookTrait : bookTraits) {
-                ChildTraits correspondingChildTrait = findChildTraitByTraitId(childTraits, bookTrait.getTrait().getTraitId());
-                double changeAmount = calculateTraitChange(bookTrait, correspondingChildTrait);
-                totalChangeAmount += changeAmount;
+        ChildTraits traits = childTraitsRepository.findChildTraitsByChildId(bookChildTraitDto.getChildId())
+            .orElseThrow(() -> NotFoundException.entityNotFound("아이 성향"));
 
-                boolean isBeyondFive = Math.abs(totalChangeAmount) >= 5.0;
-                traitsChangeDtoList.add(new TraitsChangeDto(feedbackDto.getChildId(), bookTrait.getTrait().getTraitId(), changeAmount, isBeyondFive));
+        double[] changeAmount = new double[4];
+        boolean isBeyondFive = false;
+
+        for (int i = 0; i < 4; i++) {
+            double traitChangeAmount = calculateTraitChange(bookTrait[i], childTrait[i]);
+
+            changeAmount[i] = traitChangeAmount;
+
+            if (Math.abs(traitChangeAmount) >= 5) {
+                isBeyondFive = true;
             }
-
         }
 
-        return traitsChangeDtoList;
+        return new TraitsChangeDto(
+            bookChildTraitDto.getChildId(),
+            traits.getTrait().getTraitId(),
+            changeAmount,
+            isBeyondFive
+        );
     }
 
-    private ChildTraits findChildTraitByTraitId(List<ChildTraits> childTraits, Long traitId) {
-        return childTraits.stream()
-                .filter(childTrait -> childTrait.getTrait().getTraitId().equals(traitId))
-                .findFirst().
-                orElseThrow(() -> new IllegalArgumentException("대응되는 성향이 없습니다"));
+    private static int[] calculateBookTraits(List<int[]> likedBookTraitsList, List<int[]> hatedBookTraitsList) {
+        int[] bookTraitsScore = new int[4];
+
+        // 좋아요 책 성향
+        for (int[] bookTrait : likedBookTraitsList) {
+            for (int j = 0; j < 4; j++) {
+                bookTraitsScore[j] += bookTrait[j];
+            }
+        }
+
+        // 싫어요 책 성향
+        for (int[] bookTrait : hatedBookTraitsList) {
+            for (int j = 0; j < 4; j++) {
+                bookTraitsScore[j] += bookTrait[j];
+            }
+        }
+
+        int totalBookCnt = likedBookTraitsList.size() + hatedBookTraitsList.size();
+
+        if (totalBookCnt > 0) {
+            for (int i = 0; i < 4; i++) {
+                bookTraitsScore[i] = bookTraitsScore[i] / totalBookCnt;
+            }
+        }
+
+        return bookTraitsScore;
     }
 
-    /*
-    간단하게 구현해 본 계산 로직. 이후 가중치를 심화할 수 있도록 구현
-     */
-    private double calculateTraitChange(BookTraits bookTrait, ChildTraits childTrait) {
-        double changeAmount = 0.0;
+    // 책과 아이의 성향에 따른 가중치 적용
+    private double calculateTraitChange(int bookTrait, int childTrait) {
+        final double BASE_CHANGE_AMOUNT = 0.5;
+        final double INCREASE_AMOUNT = 0.3;
+        final double DECREASE_AMOUNT = 0.3;
 
-        boolean isBookTraitLow = bookTrait.getTraitScore() < 50;
-        boolean isChildTraitLow = childTrait.getTraitScore() < 50;
+        double changeAmount = BASE_CHANGE_AMOUNT;
+
+        boolean isBookTraitLow = bookTrait < 50;
+        boolean isChildTraitLow = childTrait < 50;
 
         if (isBookTraitLow && isChildTraitLow) {
-            // 둘다 50 미만인 경우, 아이의 성향이 강화되어 더 작은 점수가 된다
-            changeAmount -= 0.5;
+            changeAmount -= BASE_CHANGE_AMOUNT;
         } else if (isBookTraitLow && !isChildTraitLow) {
-            // 책은 50 미만인데, 아이는 50 이상인 경우, 성향이 약화되어 작은 점수가 된다
-            changeAmount -= 0.3;
+            changeAmount -= DECREASE_AMOUNT;
         } else if (!isBookTraitLow && isChildTraitLow) {
-            // 책은 50 이상인데, 아이는 50 미만인 경우, 성향이 강화되어 큰 점수가 된다
-            changeAmount += 0.3;
-        } else if (!isBookTraitLow && !isChildTraitLow) {
-            // 둘다 50 이상인 경우, 성향이 강화되어 더 큰 점수가 된다
-            changeAmount += 0.5;
+            changeAmount += INCREASE_AMOUNT;
+        } else {
+            changeAmount += BASE_CHANGE_AMOUNT;
         }
 
         return changeAmount;
