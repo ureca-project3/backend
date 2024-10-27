dd721949e999fa6c4834751484925763e53ec97d
kuma0112 <lsj23871@gmail.com>
2024-10-27 12:55:47 +0900

    refactor: ChildTraitsDto 필드 변경

--- src/main/java/com/triple/backend/batch/dto/ChildTraitsDto.java
+++ src/main/java/com/triple/backend/batch/dto/ChildTraitsDto.java
@@ -1,16 +1,14 @@
 package com.triple.backend.batch.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 public class ChildTraitsDto {
-    private Long childTraitId;
-    private Long childId;
-    private Long historyId;
-    private Long traitId;
-    private String traitName;
+    private Long childTraitsId;
     private Integer traitScore;
+    private Long traitId;
 }
