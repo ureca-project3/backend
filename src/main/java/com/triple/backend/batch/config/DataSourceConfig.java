6e7a7c0cd13bad90d264f30de02c86e3be408c9b
kuma0112 <lsj23871@gmail.com>
2024-10-27 12:57:00 +0900

    refactor: DataSourceConfig 수정
    데이터베이스 2개에서 1개 사용으로 변경

--- src/main/java/com/triple/backend/batch/config/DataSourceConfig.java
+++ src/main/java/com/triple/backend/batch/config/DataSourceConfig.java
@@ -1,42 +1,29 @@
 package com.triple.backend.batch.config;
 
 import com.zaxxer.hikari.HikariDataSource;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import javax.sql.DataSource;
 
 @Configuration
-@RequiredArgsConstructor
 public class DataSourceConfig {
 
-    @Bean(name = "batchDataSource")
-    @Primary
-    @ConfigurationProperties(prefix = "spring.datasource.batch.hikari")
-    public DataSource batchDataSource() {
-        return DataSourceBuilder
-                .create()
-                .type(HikariDataSource.class)
-                .build();
+    @Bean
+    public DataSource dataSource() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/triple");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        return dataSource;
     }
 
-    @Bean(name = "mainDataSource")
-    @ConfigurationProperties(prefix = "spring.datasource.main.hikari")
-    public DataSource mainDataSource() {
-        return DataSourceBuilder
-                .create()
-                .type(HikariDataSource.class)
-                .build();
-    }
-
-    @Bean(name = "mainNamedParameterJdbcTemplate")
-    public NamedParameterJdbcTemplate mainNamedParameterJdbcTemplate(@Qualifier("mainDataSource") DataSource mainDataSource) {
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource mainDataSource) {
         return new NamedParameterJdbcTemplate(mainDataSource);
     }
 }
