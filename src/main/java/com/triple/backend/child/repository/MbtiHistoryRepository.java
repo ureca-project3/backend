6c5ebaf0d60d1339af7096d78b2df7e96dd1e22c
kuma0112 <lsj23871@gmail.com>
2024-10-28 13:41:20 +0900

    feat: findNotHavingChildTraits() 추가

--- src/main/java/com/triple/backend/child/repository/MbtiHistoryRepository.java
+++ src/main/java/com/triple/backend/child/repository/MbtiHistoryRepository.java
@@ -2,6 +2,7 @@ package com.triple.backend.child.repository;
 
 import com.triple.backend.child.entity.MbtiHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -15,5 +16,7 @@ public interface MbtiHistoryRepository extends JpaRepository<MbtiHistory, Long> 
     // MBTI 히스토리 논리적 삭제 시 히스토리 1개인지 조회
     long count();
 
-
+    @Query(value = "select mh from MbtiHistory mh left join ChildTraits ct " +
+            "on mh.historyId = ct.mbtiHistory.historyId where ct.mbtiHistory.historyId is null")
+    List<MbtiHistory> findNotHavingChildTraits();
 }
