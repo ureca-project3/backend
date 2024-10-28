bbc1ccd8505e2712ccbd6014a1719a969ff722ce
kuma0112 <lsj23871@gmail.com>
2024-10-28 14:33:39 +0900

    feat: MbtiScheduler 및 insertNewChildTraits() 생성

--- src/main/java/com/triple/backend/mbti/scheduler/MbtiScheduler.java
+++ src/main/java/com/triple/backend/mbti/scheduler/MbtiScheduler.java
@@ -1,41 +1,19 @@
 package com.triple.backend.mbti.scheduler;
 
-import com.triple.backend.child.entity.ChildTraits;
-import com.triple.backend.child.entity.MbtiHistory;
-import com.triple.backend.child.repository.ChildTraitsRepository;
-import com.triple.backend.child.repository.MbtiHistoryRepository;
+import com.triple.backend.mbti.service.MbtiService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
-@Slf4j(topic = "Batch Scheduler")
+@Slf4j(topic = "Mbti Scheduler")
 @Configuration
 @RequiredArgsConstructor
 public class MbtiScheduler {
-
-    private final MbtiHistoryRepository mbtiHistoryRepository;
-    private final ChildTraitsRepository childTraitsRepository;
+    private final MbtiService mbtiService;
 
     @Scheduled(cron = "0 0 3 * * *")
     public void insertNewChildTraits() {
-        List<MbtiHistory> mbtiHistoryList = mbtiHistoryRepository.findNotHavingChildTraits();
-        if (mbtiHistoryList != null && !mbtiHistoryList.isEmpty()) {
-            for (MbtiHistory mbtiHistory : mbtiHistoryList) {
-                List<ChildTraits> childTraitsList = childTraitsRepository.findLatestTraitsByHistoryId(mbtiHistory.getReasonId());
-                for(ChildTraits childTraits : childTraitsList) {
-                    ChildTraits NewChildTraits = ChildTraits.builder()
-                            .mbtiHistory(mbtiHistory)
-                            .trait(childTraits.getTrait())
-                            .traitScore(childTraits.getTraitScore())
-                            .createdAt(LocalDateTime.now())
-                            .build();
-                    childTraitsRepository.save(NewChildTraits);
-                }
-            }
-        }
+        mbtiService.insertNewChildTraits();
     }
 }
