25f984ecf34e448df3e2856f367de55874180f67
99MinSu <dlalstn1023@naver.com>
2024-10-28 14:50:37 +0900

    refactor: 결과 모음, 쿼리문 수정

--- src/main/java/com/triple/backend/child/repository/ChildTraitsRepository.java
+++ src/main/java/com/triple/backend/child/repository/ChildTraitsRepository.java
@@ -14,13 +14,21 @@ public interface ChildTraitsRepository extends JpaRepository<ChildTraits, Long> 
 
     @Query("SELECT new com.triple.backend.test.dto.TraitDataResponseDto(t.traitName, t.traitDescription, ct.traitScore) " +
             "FROM ChildTraits ct JOIN ct.trait t " +
-            "WHERE ct.mbtiHistory.child.childId = :childId AND ct.mbtiHistory.historyId = :historyId AND ct.trait.test.testId = :testId")
+            "WHERE ct.mbtiHistory.child.childId = :childId " +
+            "AND ct.mbtiHistory.historyId = :historyId " +
+            "AND ct.trait.test.testId = :testId " +
+            "ORDER BY ct.createdAt DESC")
     List<TraitDataResponseDto> findTraitsByChildAndTest(Long childId, Long historyId, Long testId);
 
     @Query("SELECT ct FROM ChildTraits ct JOIN FETCH ct.trait " +
             "WHERE ct.mbtiHistory.historyId = :historyId " +
             "ORDER BY ct.mbtiHistory.createdAt DESC")
     List<ChildTraits> findLatestByMbtiHistory_HistoryIdWithTraits(@Param("historyId") Long historyId);
+
+    @Query("SELECT ct FROM ChildTraits ct JOIN FETCH ct.trait " +
+            "WHERE ct.mbtiHistory.historyId = :historyId " +
+            "ORDER BY ct.trait.traitId ASC, ct.createdAt DESC")
+    List<ChildTraits> findByMbtiHistory_HistoryIdWithTraits(@Param("historyId") Long historyId);
 
     @Query("select ct from ChildTraits ct where ct.mbtiHistory.child.childId = :childId")
     Optional<ChildTraits> findByChildId(@Param(value = "childId") Long childId);
