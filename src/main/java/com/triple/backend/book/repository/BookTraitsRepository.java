d49838aca93d382d501c0b2bcacf2fa6f0f3b062
leeseunghee00 <lsh9246@gmail.com>
2024-10-28 16:35:53 +0900

    feat: 성향이 ±10 범위인 도서 조회 쿼리 작성

--- src/main/java/com/triple/backend/book/repository/BookTraitsRepository.java
+++ src/main/java/com/triple/backend/book/repository/BookTraitsRepository.java
@@ -1,8 +1,23 @@
 package com.triple.backend.book.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.triple.backend.book.entity.Book;
 import com.triple.backend.book.entity.BookTraits;
 
 public interface BookTraitsRepository extends JpaRepository<BookTraits,Long> {
+
+	@Query(value = """
+    		SELECT bt.book
+    		  FROM BookTraits bt
+    		 WHERE bt.traitScore BETWEEN :minScore AND :maxScore
+    		 ORDER BY bt.book.publishedAt DESC
+	""")
+	List<Book> findBooksByTraitScoreBetween(@Param("minScore") int minScore, @Param("maxScore") int maxScore, Pageable pageable);
+
 }
