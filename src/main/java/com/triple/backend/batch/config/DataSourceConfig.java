46bddcac1c27faa86c1065fea8553d64038f538a
KIMBUMSU123 <88920973+KIMBUMSU123@users.noreply.github.com>
2024-10-29 15:37:50 +0900

    feat: "회원 수정 및 탈퇴 (오류수정)"

--- src/main/java/com/triple/backend/batch/config/DataSourceConfig.java
+++ src/main/java/com/triple/backend/batch/config/DataSourceConfig.java
@@ -16,9 +16,9 @@ public class DataSourceConfig {
     public DataSource dataSource() {
         HikariDataSource dataSource = new HikariDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/triple");
+        dataSource.setJdbcUrl("jdbc:mysql://localhost:3307/triple");
         dataSource.setUsername("root");
-        dataSource.setPassword("31215");
+        dataSource.setPassword("root");
         return dataSource;
     }
 
