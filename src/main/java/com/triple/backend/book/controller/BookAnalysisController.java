e44fcdcff2d204ed56aa9872cae737f96d1e9c3e
KIMBUMSU123 <88920973+KIMBUMSU123@users.noreply.github.com>
2024-11-02 08:30:06 +0900

    feat: "관리자 페이지에서 MBTI 성향 생성작업 완료 "

--- src/main/java/com/triple/backend/book/controller/BookAnalysisController.java
+++ src/main/java/com/triple/backend/book/controller/BookAnalysisController.java
@@ -1,16 +1,14 @@
 package com.triple.backend.book.controller;
 
 import com.triple.backend.book.entity.Book;
+import com.triple.backend.book.repository.BookRepository;
 import com.triple.backend.book.service.BookService;
 import com.triple.backend.chatgpt.dto.BookAnalysisRequestDto;
 import com.triple.backend.common.dto.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -20,6 +18,7 @@ import java.util.Map;
 public class BookAnalysisController {
 
     private final BookService bookService;
+    private final BookRepository bookRepository;
 
     @PostMapping("/analyze/batch")
     public ResponseEntity<?> analyzeExistingBooks() {
@@ -32,12 +31,16 @@ public class BookAnalysisController {
     }
 
     @PostMapping("/analyze/mbti")
-    public ResponseEntity<?> analyzeMbti(@RequestBody Book book) {
+    public ResponseEntity<?> analyzeMbti(@RequestParam Long bookId) {
         try {
+            Book book = bookRepository.findById(bookId)
+                    .orElseThrow(() -> new IllegalArgumentException("도서 정보를 찾을 수 없습니다."));
             Map<String, Object> result = bookService.analyzeMbti(book);
             return CommonResponse.ok("MBTI Analysis Success", result);
+        } catch (IllegalArgumentException e) {
+            return CommonResponse.error(e.getMessage(), HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
-            return CommonResponse.error("MBTI Analysis Error"+ e.getMessage(), HttpStatus.BAD_REQUEST);
+            return CommonResponse.error("MBTI Analysis Error" + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
