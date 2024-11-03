8d28787f20419e8995f4d6d62dd4f5e46e99059b
kuma0112 <lsj23871@gmail.com>
2024-11-03 22:49:43 +0900

    feat: sendSlackAlert 생성
    올바른 input 데이터가 없으면 계속 에러 알람이 뜨기 때문에, 일단 주석처리하여 commit

--- src/main/java/com/triple/backend/batch/exception/CustomSkipListener.java
+++ src/main/java/com/triple/backend/batch/exception/CustomSkipListener.java
@@ -1,33 +1,36 @@
 package com.triple.backend.batch.exception;
 
 import com.triple.backend.notification.NotificationService;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.SkipListener;
+import org.springframework.context.annotation.Bean;
 
 import java.io.IOException;
 
 @Slf4j
+@AllArgsConstructor
 @RequiredArgsConstructor
 public class CustomSkipListener implements SkipListener<Object, Object> {
 
-    private final String stepName;
+    private String stepName;
     private final NotificationService notificationService;
 
     @Override
     public void onSkipInRead(Throwable t) {
         log.error("{} 읽기 작업 도중 문제가 발생했습니다. 해당 아이템을 스킵합니다", stepName, t);
-        sendSlackAlert(t);
+//        sendSlackAlert(t);
     }
 
     public void onSkipInWrite(Object item, Throwable t) {
         log.error("{} 쓰기 작업 도중 문제가 발생했습니다. 해당 아이템을 스킵합니다 : {}", stepName, item, t);
-        sendSlackAlert(t);
+//        sendSlackAlert(t);
     }
 
     public void onSkipInProcess(Object item, Throwable t) {
         log.error("{} 프로세스 작업 도중 문제가 발생했습니다. 해당 아이템을 스킵합니다: {}", stepName, item, t);
-        sendSlackAlert(t);
+//        sendSlackAlert(t);
     }
 
     private void sendSlackAlert(Throwable t) {
